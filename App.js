d6c8baf9834f167a537e716a4e3dcacfe6138bed
Vadim Egorov <43100771+vadimegorov13@users.noreply.github.com>
2022-02-05 07:35:17 -0900

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { Camera } from 'expo-camera';
+
+import { StyleSheet, Text, View, Pressable } from 'react-native';
 
 export default function App() {
+  const cameraRef = useRef();
+  const [hasPermission, setHasPermission] = useState(null);
+
+  const handleImageCapture = async () => {
+    const imageData = await cameraRef.current.takePictureAsync({
+      base64: true,
+    });
+
+    console.log('image taken');
+  };
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Camera
+        ref={cameraRef}
+        style={styles.cameraView}
+        type={Camera.Constants.Type.back}
+        autoFocus={true}
+        whiteBalance={Camera.Constants.WhiteBalance.auto}
+      ></Camera>
+
+      <Pressable
+        onPress={() => handleImageCapture()}
+        style={styles.button}
+      ></Pressable>
     </View>
   );
 }
@@ -16,5 +54,25 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  cameraView: {
+    height: '100%',
+    // left: Dimensions.get('screen').width / 2 - 50,
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'transparent',
+  },
+
+  button: {
+    position: 'absolute',
+    color: '#00FF00',
+    bottom: 40,
+    width: 300,
+    zIndex: 100,
+    height: 100,
+    backgroundColor: 'white',
+    borderRadius: 40,
   },
 });
